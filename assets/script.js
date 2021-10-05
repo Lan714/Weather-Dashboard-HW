7188bf1917a7facfcd4a94aa5494ee2f8759f709
Lan Bui <lantbui27@gmail.com>
2021-10-05 11:50:23 -0700

    functions

--- assets/script.js
+++ assets/script.js
@@ -17,8 +17,40 @@ document.getElementById('searchBtn').addEventListener('click', event => {
     cities.push(cityName)
     localStorage.setItem('cities', JSON.stringify(cities))
   }
-  
+
+renderCities(cities, cityName)
+getWeather(cityName)
+lastCity = cityName
+localStorage.setItem('last', lastCity)
+document.getElementById('city').value=''
+
 })
+
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function renderCities(cities, current) {
+  let list = document.getElementById('cityList')
+  removeAllChildNodes(list)
+
+  cities.forEach(element => {
+    let city = document.createElement('button')
+    city.type = 'button'
+    if (current.toUpperCase() === element.toUpperCase()) {
+      city.className = "list-group-item list-group-item-action active city"
+    } else {
+      city.className = "list-group-item list-group-item-action city"
+    }
+    city.innerHTML = element
+    list.append(city)
+  });
+
+}
+
 
 
 const API = 6526862a1809d80a03753664a86c2445
